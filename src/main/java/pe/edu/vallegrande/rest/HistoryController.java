98f2e3195edddbcc86866a048c456ca418bc6644
Cardenas Trujillo Alonso <hancel.cardenas.trujillo@vallegrande.edu.pe>
2026-05-11 10:28:54 -0500

    feat: add get history

---
+++ src/main/java/pe/edu/vallegrande/rest/HistoryController.java
@@ -0,0 +1,29 @@
+package pe.edu.vallegrande.rest;
+
+import org.springframework.web.bind.annotation.*;
+import pe.edu.vallegrande.model.ApiResponse;
+import pe.edu.vallegrande.repository.ApiRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1/responses")
+@CrossOrigin(origins = "*") // Importante para que Angular no de error
+public class HistoryController {
+
+    private final ApiRepository apiRepository;
+
+    public HistoryController(ApiRepository apiRepository) {
+        this.apiRepository = apiRepository;
+    }
+
+    @GetMapping
+    public Flux<ApiResponse> getAll() {
+        return apiRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> delete(@PathVariable String id) {
+        return apiRepository.deleteById(id);
+    }
+}
